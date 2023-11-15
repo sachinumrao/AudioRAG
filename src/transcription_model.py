ce21ca80cdb61b9479d9118604eacc0111ec370d
Sachin Umrao <sachin.umrao.1512@gmail.com>
2023-11-15 09:00:48 +0530

    base code for distil whisper model for audio transcription

---
+++ src/transcription_model.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+
+
+class AudioModel:
+    def __init__(self):
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+        self.batch_size = 1
+
+        model_id = "distil-whisper/distil-medium.en"
+
+        self.model = AutoModelForSpeechSeq2Seq.from_pretrained(
+            model_id, torch_dtype=self.torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+        )
+        self.model.to(self.device)
+        
+        self.model = self.model.to_bettertransformer()
+
+        self.processor = AutoProcessor.from_pretrained(model_id)
+
+        self.pipe = pipeline(
+            "automatic-speech-recognition", 
+            model=self.model,
+            tokenizer=self.processor.tokenizer,
+            feature_extractor=self.processor.feature_extractor,
+            max_new_tokens=128,
+            chunk_length_s=15,
+            batch_size=self.batch_size,
+            torch_dtype=self.torch_dtype,
+            device=self.device,
+        )
+
+    def get_transcript(self, audio_filepath):
+        result = self.pipe(audio_filepath)
+        return result
+    
+    
+    
+def main():
+    
+    audio_directory = "./../audio/"
+    audio_filename = ""
+    audio_filepath = Path(audio_directory, audio_filename)
+    
+    model = AudioModel()
+    transcript = model.get_transcript()
+    pass
+
+
+if __name__ == "__main__":
+    main()
