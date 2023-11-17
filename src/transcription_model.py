1aae660507a5246464403b4407999f7dc15b5b3b
Sachin Umrao <sachin.umrao.1512@gmail.com>
2023-11-17 10:56:24 +0530

    switched to using whisper v1 tiny model

--- src/transcription_model.py
+++ src/transcription_model.py
@@ -1,3 +1,4 @@
+import time
 from pathlib import Path
 
 import torch
@@ -7,53 +8,50 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 class AudioModel:
     def __init__(self):
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        self.torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+        self.torch_dtype = (
+            torch.float16 if torch.cuda.is_available() else torch.float32
+        )
         self.batch_size = 1
 
-        model_id = "distil-whisper/distil-medium.en"
-        
         print("Downloading whisper model...")
 
-        self.model = AutoModelForSpeechSeq2Seq.from_pretrained(
-            model_id, torch_dtype=self.torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
-        )
-        
-        print("Model download complete...")
-        self.model.to(self.device)
-        
-        self.model = self.model.to_bettertransformer()
-
-        self.processor = AutoProcessor.from_pretrained(model_id)
-
         self.pipe = pipeline(
-            "automatic-speech-recognition", 
-            model=self.model,
-            tokenizer=self.processor.tokenizer,
-            feature_extractor=self.processor.feature_extractor,
-            max_new_tokens=128,
+            "automatic-speech-recognition",
+            model="openai/whisper-tiny.en",
             chunk_length_s=15,
-            batch_size=self.batch_size,
-            torch_dtype=self.torch_dtype,
             device=self.device,
         )
+        print("Model download complete...")
 
     def get_transcript(self, audio_filepath):
         print(f"Transcribing file: {audio_filepath}")
         result = self.pipe(audio_filepath)
-        return result
-    
-    
-    
+        return result["text"]
+
+
 def main():
-    
     audio_directory = "./../audio/"
     audio_filename = "State_of_GPT_|_BRK216HFS.mp3"
     audio_filepath = Path(audio_directory, audio_filename)
-    
+
+    t1 = time.perf_counter()
     model = AudioModel()
+    t2 = time.perf_counter()
+
+    print("Time taken to load the model: ", t2 - t1)
+
+    t3 = time.perf_counter()
     transcript = model.get_transcript(str(audio_filepath))
-    print("Transcript:")
-    print(transcript)
+    t4 = time.perf_counter()
+
+    print("Time taken for transcription: ", t4 - t3)
+
+    print("Words in transcript: ", len(transcript.split()))
+
+    output_path = Path("./../transcripts/", "karpathy_state_of_gpt.txt")
+
+    with open(output_path, "w+") as f:
+        f.writelines(transcript)
 
 
 if __name__ == "__main__":
